f80ae163615458d7cdd88e8e5e8b17e8768ae1e7
Nicholas Talbot <talbotnic26@gmail.com>
2023-01-26 11:49:15 -0700

    no longer breaks on item page

--- react-app/src/components/ItemPage/index.js
+++ react-app/src/components/ItemPage/index.js
@@ -6,7 +6,7 @@ import './ItemPage.css'
 import ReviewForm from "../ReviewForm";
 import OpenModalButton from "../OpenModalButton";
 import UpdateReview from "../UpdateReview";
-import { deleteSingleReview, getAllReviews } from "../../store/review";
+import { getAllReviews } from "../../store/review";
 import DeleteReview from "../DeleteReview";
 
 export default function ItemPage() {
@@ -14,7 +14,7 @@ export default function ItemPage() {
   const { itemId } = useParams()
   const dispatch = useDispatch()
   const item = useSelector(state => state.items[itemId])
-  const userId = useSelector(state => state.session.user.id)
+  const user = useSelector(state => state.session.user)
 
   useEffect(() => {
     dispatch(getItemById(itemId))
@@ -24,6 +24,47 @@ export default function ItemPage() {
 
   if (!loaded) return null
   if (!item) return null
+
+  if (!user) {
+    return (
+      <div className="targét-item-container">
+        <div className="top-half">
+        <h1 className="targét-item-header">{item.name}</h1>
+        <div className="targét-item-content-container">
+          <img src={item.imageUrl} alt='targét-item' className="targét-item-picture" />
+          <div className="targét-item-info-container">
+            <h2>${item.price}</h2>
+            <div>Quantity and Add to cart Placeholder</div>
+          </div>
+        </div>
+        </div>
+        <div className="middle-part">
+            <p>{item.description}</p>
+        </div>
+        <div className="bottom-half">
+            <OpenModalButton
+              buttonText='Write Review'
+              modalComponent={<ReviewForm itemId={itemId} />}
+              className='write-review-button'
+              />
+            <div>{item.reviews.length ? item.reviews.map(review => (
+              <div key={review.id} className='review-card-container'>
+              <div className="review-content-container">
+                <h4 className="review-h4">{review.title}</h4>
+                <div>{review.rating}</div>
+                <span className="tiny-review-name">{review.user.firstName}</span>
+                <div>{review.review}</div>
+              {review.imageUrl ? (
+              <img src={review.imageUrl} alt='review' className="review-image" />
+                ) : null }
+              </div>
+              </div>
+            )) : "No Reviews for this product yet."}
+            </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className="targét-item-container">
@@ -56,7 +97,7 @@ export default function ItemPage() {
             {review.imageUrl ? (
             <img src={review.imageUrl} alt='review' className="review-image" />
               ) : null }
-              { review.userId === userId ?
+              { review.userId === user.id ?
               <>
               <OpenModalButton
               buttonText='Edit review'
