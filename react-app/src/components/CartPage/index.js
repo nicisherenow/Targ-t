caa9783fb147d2c98f5849465b924559a03f520b
Nicholas Talbot <talbotnic26@gmail.com>
2023-01-27 11:40:02 -0700

    got the necessary data for cart

--- react-app/src/components/CartPage/index.js
+++ react-app/src/components/CartPage/index.js
@@ -2,23 +2,37 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import OpenModalButton from "../OpenModalButton";
 import LoginForm from "../auth/LoginForm";
+import './CartPage.css'
 
 
 export default function CartPage() {
   const [loaded, setLoaded] = useState(false)
   // const dispatch = useDispatch()
   const user = useSelector(state => state.session.user)
+  const carts = useSelector(state => state.carts)
 
+  let cartsList;
+  let totalPrice = 0;
+  if (carts) {
+    cartsList = Object.values(carts)
+    cartsList.forEach(cart => {
+      totalPrice += cart.item.price * cart.quantity
+    })
+  }
+  console.log(totalPrice)
+  let tax = totalPrice * 8.25/100
+  console.log(tax)
+  let priceWithTax = totalPrice += tax
+  console.log(+priceWithTax.toFixed(2))
   useEffect(()=> {
     setLoaded(true)
   }, [loaded])
 
-  if (!loaded) return null
 
   return (
     <>
-    { !user ?
-      <div>
+    { !user || !carts || !loaded ?
+      <div className="cart-page-body">
         <h1>Your cart is empty</h1>
         <h3>Have an account? Sign in to see your cart</h3>
         <OpenModalButton
@@ -27,7 +41,15 @@ export default function CartPage() {
           className='sign-in-button'
           />
       </div>
-      : null
+      : <div className="cart-page-body">
+        {cartsList.map(cart => (
+          <div className="cart-item-container">
+            <div>{cart.item.name}</div>
+            <div>{cart.quantity}</div>
+            <div>{cart.item.price}</div>
+          </div>
+        ))}
+      </div>
     }
     </>
   )
