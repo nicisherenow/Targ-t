dcc8b783421a9931d6e91e3a5e3783cc9810ea61
Nicholas Talbot <talbotnic26@gmail.com>
2023-01-28 20:57:14 -0700

    looking nice at home page

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -4,14 +4,36 @@ import { getAllItems } from "../../store/item";
 import { NavLink } from "react-router-dom"
 import './HomePage.css'
 import { getAllReviews } from "../../store/review";
+import { createNewCart, getAllCarts } from "../../store/cart";
 
 export default function HomePage() {
   const [loaded, setLoaded] = useState(false)
+  const [category, setCategory] = useState('')
+  const [itemId, setItemId] = useState(0)
   const dispatch = useDispatch()
-  const items = Object.values(useSelector(state => state.items))
+  const items = useSelector(state => state.items)
+  const user = useSelector(state => state.session.user)
 
+  let itemsList;
+  if (items) {
+    itemsList = Object.values(items)
+  }
 
-  const item = items[0]
+  let categoryList;
+  if (itemsList) {
+    categoryList = itemsList.filter(item => item.category === category)
+  }
+
+  const addToCart = async (e) => {
+    e.preventDefault()
+    await dispatch(createNewCart(user.id, +itemId, 1))
+    await dispatch(getAllCarts())
+  }
+
+  const updateItemId = (e) => {
+    setItemId(e.target.value)
+  }
+  const item = itemsList[0]
 
   useEffect(() => {
     dispatch(getAllItems())
@@ -26,9 +48,9 @@ export default function HomePage() {
     <div className="targét-home-container">
       <div className="targét-home-content-container">
         <div className="targét-home-words">
-        <h1 className="targét-home-header">Welcome to Targét</h1>
-        <h2>Tired of buying the same ol' everyday items at reasonable prices?</h2>
-        <h3>Where everything is just a little bit more expensive and that's ok because it's french.</h3>
+          <h1 className="targét-home-header">Welcome to Targét</h1>
+          <h2>Tired of buying the same ol' everyday items at reasonable prices?</h2>
+          <h3>Where everything is just a little bit more expensive and that's ok because it's french.</h3>
         </div>
         <div className="pic-home-nav-container">
           <NavLink to={`/items/${item.id}`}>
@@ -36,6 +58,31 @@ export default function HomePage() {
           </NavLink>
         </div>
       </div>
+      <h4 className="category-header">{category ? category : "We've got it all!"}</h4>
+        <div className="category-holder">
+        {category ? categoryList.map(item => (
+          <div className='category-containers'>
+          <NavLink className='category-navs' to={`/items/${item.id}`} key={item.id}>
+            <img src={item.imageUrl} alt={item.name} />
+            <div>${item.price}</div>
+          </NavLink>
+        </div>
+        )) :
+        itemsList.map(item => (
+        <div className='category-containers'>
+          <NavLink className='category-navs' to={`/items/${item.id}`} key={item.id}>
+            <img src={item.imageUrl} alt={item.name} className='home-shopping-image' />
+            <div>${item.price}</div>
+          </NavLink>
+          {user ?
+          <button onClick={addToCart} onMouseEnter={updateItemId} value={item.id} className='home-cart-button'>Add to cart</button>
+            : null
+        }
+        </div>
+        ))
+
+        }
+        </div>
     </div>
   )
 }
