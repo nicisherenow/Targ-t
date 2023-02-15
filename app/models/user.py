b4f0f41d7a204ec1df07699f6b2d677dd9a99fc3
Nicholas Talbot <talbotnic26@gmail.com>
2023-02-15 15:21:28 -0700

    fixed wishlist css

--- app/models/user.py
+++ app/models/user.py
@@ -170,3 +170,15 @@ class Wishlist(db.Model):
             'userId': self.item_id,
             'item': self.item.to_dict()
         }
+
+# class Purchased(db.Model):
+#     __tablename__ = 'purchased'
+
+#     if environment == "production":
+#         __table_args__ = {'schema': SCHEMA}
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     item_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('items.id')), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+
+# class ItemsPurchased(db.Model):
